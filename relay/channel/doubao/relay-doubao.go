2e682277c43cd41359758947ede97b41e240b733
linzhaoming <zhaominglam@gmail.com>
2024-08-01 12:17:04 +0800

    feat: modify judgement

--- relay/channel/doubao/relay-doubao.go
+++ relay/channel/doubao/relay-doubao.go
@@ -3,11 +3,12 @@ package doubao
 import (
 	"fmt"
 	relaycommon "one-api/relay/common"
+	"strings"
 )
 
 func GetRequestURL(info *relaycommon.RelayInfo) (string, error) {
 	// Support BotChatCompletions
-	if info.RequestModelName == "Doubao-bot-chat" {
+	if strings.HasPrefix(info.RequestModelName, "Doubao-bot-chat") {
 		return fmt.Sprintf("%s/api/v3/bots/chat/completions", info.BaseUrl), nil
 	}
 	return fmt.Sprintf("%s/api/v3/chat/completions", info.BaseUrl), nil
